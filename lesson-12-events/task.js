65a439fa4902e8d8c86fac933054286f0fd940ea
Radkevich Liudmila <gamaleva@tut.by>
2018-10-22 00:25:14 +0300

    Fixed

--- lesson-12-events/task.js
+++ lesson-12-events/task.js
@@ -1,72 +1,54 @@
 'use strict';
 
 (function imageValue() {
-  var DragImage = null;
   var DragShiftX;
   var DragShiftY;
   var elements = document.getElementsByTagName('img');
   
-  for (var i = 0; i < elements.length; i++) {
+  for (var i = elements.length - 1; i >= 0; i--) {
     elements[i].style.left = elements[i].offsetLeft + 'px';
     elements[i].style.top = elements[i].offsetTop + 'px';
     elements[i].style.zIndex = '0';
-  }
-  for (i = 0; i < elements.length; i++) {
     elements[i].style.position = 'absolute';
+    elements[i].addEventListener('mousedown', Drag_Start, false);
   }
 
-  document.querySelector('div').addEventListener('mousedown', function (EO) {
+  function Drag_Start(EO) {   
     EO = EO || window.event;
-    DragImage = EO.target.id;
-    var imageObj = document.getElementById(DragImage);
-    imageObj.addEventListener('mousedown', Drag_Start, false);
-    document.querySelector('div').addEventListener('mousemove', Drag_Move, false);
-    document.querySelector('div').addEventListener('mouseup', Drag_Stop, false);
-  });
-  
-  function Drag_Start(EO) {
-    EO = EO || window.event;
+    const DragImage = EO.target;
     var mouseX = EO.pageX;
     var mouseY = EO.pageY;
-    var imageObj = document.getElementById(DragImage);
-    var imageX = imageObj.offsetLeft;
-    var imageY = imageObj.offsetTop;
+    var imageX = DragImage.offsetLeft;
+    var imageY = DragImage.offsetTop;
     DragShiftX = mouseX - imageX;
     DragShiftY = mouseY - imageY;
-
-    for (i = 0; i < elements.length; i++) {
-      elements[i].style.zIndex = '0';
-    }
-    imageObj.style.zIndex = '1';
+    window.addEventListener('mousemove', Drag_Move, false);
+    window.addEventListener('mouseup', Drag_Stop, false);
+    window.removeEventListener('mouseout', Drag_Stop, false);
   };
 
   function Drag_Move(EO) {
     if (DragShiftX && DragShiftY) {
       EO = EO || window.event;
+      EO.preventDefault();
+      const DragImage = EO.target;
       var mouseX = EO.pageX;
       var mouseY = EO.pageY;
-      var imageObj = document.getElementById(DragImage);
+      DragImage.style.zIndex = '1';
       var imageX = mouseX - DragShiftX;
       var imageY = mouseY - DragShiftY;
-      imageObj.style.left = imageX + 'px';
-      imageObj.style.top = imageY + 'px';
+      DragImage.style.left = imageX + 'px';
+      DragImage.style.top = imageY + 'px';
     };
   };
 
   function Drag_Stop(EO) {
-    if (DragShiftX && DragShiftY) {
-      EO = EO || window.event;
-      var mouseX = EO.pageX;
-      var mouseY = EO.pageY;
-      var imageObj = document.getElementById(DragImage);
-      var imageX = mouseX - DragShiftX;
-      var imageY = mouseY - DragShiftY;
-      imageObj.style.left = imageX + 'px';
-      imageObj.style.top = imageY + 'px';
-      DragImage = null;
-      DragShiftX = null;
-      DragShiftY = null;
-    };
+    EO = EO || window.event;
+    EO.preventDefault();
+    const DragImage = EO.target;
+    DragImage.style.zIndex = '0';
+    window.removeEventListener('mousemove', Drag_Move, false);
+    window.removeEventListener('mouseup', Drag_Stop, false);
+    window.removeEventListener('mouseout', Drag_Stop, false);
   };
-
 })();
