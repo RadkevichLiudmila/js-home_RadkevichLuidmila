76a72b48b4413f9e61eb54704c85d0eb95ae9dd4
Radkevich Liudmila <gamaleva@tut.by>
2018-11-12 17:00:20 +0300

    Change task

--- lesson-20-ajax-spa/AJAXStorage.js
+++ lesson-20-ajax-spa/AJAXStorage.js
@@ -1,14 +1,14 @@
 "use strict";
 
 function TAJAXStorage(name) {
-    var self = this;
-    self.name = name;
-    self.myName = 'Radkevich_DRINKS';
-    self.storage = {};
-    
-    var AjaxHandlerScript = "http://fe.it-academy.by/AjaxStringStorage2.php";
+  var self = this;
+  self.name = name;
+  self.myName = 'Radkevich_DRINKS';
+  self.storage = {};
 
-    // забрали данные с сервера
+  var AjaxHandlerScript = "http://fe.it-academy.by/AjaxStringStorage2.php";
+
+  // забрали данные с сервера
   self.readStorage = function() {
     $.ajax(
       {
@@ -21,87 +21,88 @@ function TAJAXStorage(name) {
       }
     );
   }
-      
-    // сохранили данные в массив
-    function writeReady(ResultH) {
-      self.storage = JSON.parse(ResultH.result);
-    }
+
+  // сохранили данные в массив
+  function writeReady(ResultH) {
+    self.storage = JSON.parse(ResultH.result);
+  }
 
   self.readStorage();
-    
+
   // подготавливаем сервер к изменениям
   self.trainingToChange = function() {
     self.updatePassword = Math.random();
-    $.ajax(
-      {
-        url: AjaxHandlerScript,
-        type: 'POST',
-        data: {
-          f: 'LOCKGET', n: self.myName,
-          p: self.updatePassword
-        },
-        cache: false,
-        success: (obj) => console.log(obj),
-        error: self.ErrorHandler
-      }
-    );
+    $.ajax({
+      url: AjaxHandlerScript,
+      type: 'POST',
+      data: {
+        f: 'LOCKGET', n: self.myName,
+        p: self.updatePassword
+      },
+      cache: false,
+      success: function(obj) {
+        console.log(obj);
+      },
+      error: self.ErrorHandler
+    });
   }
 
   // посылаем измененный массив на сервер
   self.Update = function() {
-  
-    $.ajax( {
-      url : AjaxHandlerScript, 
-      type : 'POST', 
-      data: {f: 'UPDATE', n: self.myName, 
+
+    $.ajax({
+      url : AjaxHandlerScript,
+      type : 'POST',
+      data: {f: 'UPDATE', n: self.myName,
           v : JSON.stringify(self.storage), p: self.updatePassword},
       cache: false,
-      success: (obj) => console.log(obj), 
+      success:  function(obj) {
+        console.log(obj);
+      },
       error: self.ErrorHandler
-      }
-    );
+    });
   }
 
-//-----------------------------------------------
-    self.addValue = function(key, value) {
-      
-      self.trainingToChange();
-      self.storage[key] = value;
+  //-----------------------------------------------
+  self.addValue = function(key, value) {
+
+    self.trainingToChange();
+    self.storage[key] = value;
+    self.Update();
+
+  };
+
+  self.getValue = function (key) {
+
+    self.readStorage();
+    return self.storage[key];
+
+  };
+
+  self.deleteValue = function(key) {
+
+    self.trainingToChange();
+    if (self.storage[key]) {
+      delete self.storage[key];
       self.Update();
-
-    };
-  
-    self.getValue = function (key) {
-
-      self.readStorage();
-      return self.storage[key];
-
-    };
-
-    self.deleteValue = function(key) {
-
-     self.trainingToChange();
-      if (self.storage[key]) {
-        delete self.storage[key];
-        self.Update();
-        return true;
-      } else {
-        return false;
-      }
-    };
-
-    self.getKeys = function () {
-
-      self.readStorage();
-      var keys = [];
-      for (var i in self.storage) {
-        keys.push(i);
-      }
-      return keys;
-
+      return true;
+    } else {
+      return false;
     }
+  };
 
-    self.ErrorHandler =  function (jqXHR, StatusStr, ErrorStr) {
-        alert(StatusStr + ' ' + ErrorStr);
+  self.getKeys = function () {
+
+    self.readStorage();
+    var keys = [];
+    for (var i in self.storage) {
+      keys.push(i);
     }
+    return keys;
+
   }
+
+  self.ErrorHandler =  function (jqXHR, StatusStr, ErrorStr) {
+    alert(StatusStr + ' ' + ErrorStr);
+  }
+}
