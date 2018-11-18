c88a645bb9cfdfe62e11726f578be6e3efb308e1
Radkevich Liudmila <gamaleva@tut.by>
2018-11-18 13:53:44 +0300

    Fixed

--- lesson-15-animation/task.js
+++ lesson-15-animation/task.js
@@ -6,7 +6,6 @@ const sizeBall = 30; // размер шарика
 const racketWidth = parseInt(fieldWidth / 40); // ширина ракетки
 const racketHeight = parseInt(fieldHeight / 3); // высота ракетки
 
-document.getElementsByTagName('body')[0].id = 'body';
 let body = document.getElementById('body');
 body.appendChild(createContainer());
 
@@ -61,7 +60,7 @@ function createRacketTwo() {
 }
 
 //-----------------------------------------------------------
-let field = {
+let playingField = {
   Width: fieldWidth,
   Height: fieldHeight,
 
@@ -81,7 +80,7 @@ let goal = {
   }
 }
 
-function DescriptionOfObject(Width, Height, PosX, PosY, SpeedX, SpeedY, Id) {
+function PlayingBall(Width, Height, PosX, PosY, SpeedX, SpeedY, Id) {
   this.PosX = PosX;
   this.PosY = PosY;
   this.Width = Width;
@@ -97,18 +96,34 @@ function DescriptionOfObject(Width, Height, PosX, PosY, SpeedX, SpeedY, Id) {
     Obj.style.transform = 'translateZ(0)';
   }
 }
- 
-let ball = new DescriptionOfObject(sizeBall, sizeBall, fieldWidth / 2 - sizeBall / 2, fieldHeight / 2 - sizeBall / 2, 0, 0, 'ball'); 
-let racket1 = new DescriptionOfObject(racketWidth, racketHeight, 0, fieldHeight / 2 - racketHeight / 2, 0, 0, 'racket1');
-let racket2 = new DescriptionOfObject(racketWidth, racketHeight, fieldWidth - racketWidth, fieldHeight / 2 - racketHeight / 2, 0, 0, 'racket2');
+function Racket(Width, Height, PosX, PosY, SpeedX, SpeedY, Id) {
+  this.PosX = PosX;
+  this.PosY = PosY;
+  this.Width = Width;
+  this.Height = Height;
+  this.SpeedX = SpeedX;
+  this.SpeedY = SpeedY;
+  this.Update = function () {
+    let Obj = document.getElementById(Id);
+    Obj.style.width = this.Width + 'px';
+    Obj.style.height = this.Height + 'px';
+    Obj.style.left = this.PosX + 'px';
+    Obj.style.top = this.PosY + 'px';
+    Obj.style.transform = 'translateZ(0)';
+  }
+}
+
+let ball = new PlayingBall(sizeBall, sizeBall, fieldWidth / 2 - sizeBall / 2, fieldHeight / 2 - sizeBall / 2, 0, 0, 'ball');
+let racket1 = new Racket(racketWidth, racketHeight, 0, fieldHeight / 2 - racketHeight / 2, 0, 0, 'racket1');
+let racket2 = new Racket(racketWidth, racketHeight, fieldWidth - racketWidth, fieldHeight / 2 - racketHeight / 2, 0, 0, 'racket2');
 
 function Tick() {
-  
+
   ball.PosX += ball.SpeedX;
   // проверка, отбился ли мячик от левой ракетки
   if ((ball.PosX < racket1.Width) &&
-    (ball.PosY - ball.Height / 2 > racket1.PosY) &&
-    (ball.PosY + ball.Height / 2 < racket1.PosY + racket1.Height)){
+    (ball.PosY + ball.Height / 2 > racket1.PosY) &&
+    (ball.PosY + ball.Height / 2 < racket1.PosY + racket1.Height)) {
     ball.SpeedX = -ball.SpeedX;
     ball.PosX = racket1.Width;
   } else if (ball.PosX < 0) { // ограничение слева
@@ -120,15 +135,15 @@ function Tick() {
   }
 
   // проверка, отбился ли мячик от правой ракетки
-  if ((ball.PosX + ball.Width > field.Width - racket2.Width) &&
-    (ball.PosY - ball.Height / 2 > racket2.PosY) &&
-    (ball.PosY + ball.Height / 2 < racket2.PosY + racket2.Height)){
+  if ((ball.PosX + ball.Width > playingField.Width - racket2.Width) &&
+    (ball.PosY + ball.Height / 2 > racket2.PosY) &&
+    (ball.PosY + ball.Height / 2 < racket2.PosY + racket2.Height)) {
     ball.SpeedX = -ball.SpeedX;
-    ball.PosX = field.Width - ball.Width - racket2.Width;
-  } else if (ball.PosX + ball.Width > field.Width) { // ограничение справа
+    ball.PosX = playingField.Width - ball.Width - racket2.Width;
+  } else if (ball.PosX + ball.Width > playingField.Width) { // ограничение справа
     ball.SpeedX = 0;
     ball.SpeedY = 0;
-    ball.PosX = field.Width - ball.Width;
+    ball.PosX = playingField.Width - ball.Width;
     goal.player1++;
     goal.Update();
   }
@@ -140,9 +155,9 @@ function Tick() {
     ball.PosY = 0;
   }
   // ограничение снизу
-  if (ball.PosY + ball.Height > field.Height) {
+  if (ball.PosY + ball.Height > playingField.Height) {
     ball.SpeedY = -ball.SpeedY;
-    ball.PosY = field.Height - ball.Height;
+    ball.PosY = playingField.Height - ball.Height;
   }
 
    // движение левой ракетки + ограничение сверху и снизу
@@ -151,9 +166,9 @@ function Tick() {
     racket1.SpeedY = 0;
     racket1.PosY = 0;
   }
-  if (racket1.PosY + racket1.Height >  field.Height) {
+  if (racket1.PosY + racket1.Height >  playingField.Height) {
     racket1.SpeedY = 0;
-    racket1.PosY = field.Height - racket1.Height;
+    racket1.PosY = playingField.Height - racket1.Height;
   }
   // движение правой ракетки + ограничение сверху и снизу
   racket2.PosY += racket2.SpeedY;
@@ -161,9 +176,9 @@ function Tick() {
     racket2.SpeedY = 0;
     racket2.PosY = 0;
   }
-  if (racket2.PosY + racket2.Height >  field.Height) {
+  if (racket2.PosY + racket2.Height >  playingField.Height) {
     racket2.SpeedY = 0;
-    racket2.PosY = field.Height - racket2.Height;
+    racket2.PosY = playingField.Height - racket2.Height;
   }
   // проверяем может кто-то уже выиграл
   if (goal.player1 === 10 || goal.player2 === 10) {
@@ -223,9 +238,9 @@ function attachedSpeed() {
   ball.SpeedY = Math.random() * 30 - 15;
 }
 
-// считаем ускорение для мячика по X. Диапозон от -25 до + 25. исключаем промежуток -15 - +15 
-//т к, если скорость по Х будет меньше скорости по Y (скорость по Y -15 - +15), 
-//то мячик будет лететь больше вверх/вниз, а нам надо вправо/влево. 
+// считаем ускорение для мячика по X. Диапозон от -25 до + 25. исключаем промежуток -15 - +15
+//т к, если скорость по Х будет меньше скорости по Y (скорость по Y -15 - +15),
+//то мячик будет лететь больше вверх/вниз, а нам надо вправо/влево.
 //Тем более, если скорость по Х будет стремиться к 0, то мячик будет двигаться очень медленно. Заодно исключаем и это.
 function calcSpeedX() {
   let x = (Math.random() * 50) - 25;
@@ -237,7 +252,7 @@ function calcSpeedX() {
   return x;
 }
 
-field.Update();
+playingField.Update();
 ball.Update();
 racket1.Update();
 racket2.Update();
